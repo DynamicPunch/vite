d5e3b20d537379145c3ee7583771ad400f306f5b
DynamicPunch <michaelgardner231@gmail.com>
2023-02-13 13:12:45 -0500

    Added Card

--- src/App.vue
+++ src/App.vue
@@ -19,4 +19,134 @@ import HelloWorld from './components/HelloWorld.vue'
   color: #2c3e50;
   margin-top: 60px;
 }
+
+
+.s1 {
+  width: 350px;
+  text-align: center;
+  background: pink;
+
+  
+  margin-top: 16px;
+  margin-bottom: 16px;
+  margin-left: 16px;
+  margin-right: 16px;
+  
+  padding-top: 8px;
+  padding-bottom: 8px;
+  padding-left: 8px;
+  padding-right: 8px;
+  
+  border: 2px solid red;
+  border-radius: 16px;
+  box-shadow: 4px 4px;
+}
+
+
+.h1 {
+  font: 28px Comic Sans MS;
+  color: white;
+  text-shadow: 2px 2px #000000;
+  line-height: 0px;
+  
+}
+.h2 {
+  font: 20px Comic Sans MS;
+  color: white;
+  margin-bottom: 16px;
+  text-shadow: 2px 2px #000000;
+}
+
+.p1 {
+  font: 14px Comic Sans MS;
+  color: white;
+  margin-top: 16px;
+  margin-left: 16px;
+  margin-right: 16px;
+  text-shadow: 2px 2px #000000;
+}
+
+.l1 {
+  font: 14px Comic Sans MS;
+  color: white;
+  margin-bottom: 24px;
+  margin-left: 16px;
+  margin-right: 16px;
+  text-shadow: 2px 2px #000000;
+  
+}
+
+ul {
+    text-align: center;
+    list-style-position: inside;
+}
+
+
+.b1 {
+  font: 12px Comic Sans MS;
+  border-radius: 16px;
+  box-shadow: 2px 2px;
+  margin-bottom: 16px;
+  
+}
+
+.i1 {
+  border: 3px solid blue;
+  border-radius: 16px;
+  box-shadow: 4px 4px;
+  
+}
+
+
+@media (max-width: 800px) and (min-width: 500px) {
+  .b1 {
+    display: none;
+  }
+}
+
+@media (max-width: 500px) {
+  * {
+    transform: scale(0.9);
+  }
+}
+
+body {
+  background: cyan;
+}
+
+
+ .but{
+   padding: 4px;
+       box-shadow: 2px 2px #000000;
+    background-color: cyan;
+     border-radius: 16px;
+     border: 2px solid blue;
+  text-shadow: 2px 2px #000000;
+    font: 14px Comic Sans MS;
+  color: white;
+
+}
+
+.but:hover{
+  background-color: red;
+}
+
+.but:focus {
+  background-color: blue; 
+}
+
+
+.tog
+{
+  background-color: cyan;
+}
+
+.tog2
+{
+  background-color: pink;
+}
+
+
+
+
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,44 @@
 <template>
-  <h1>{{ msg }}</h1>
+ <div className="App">
 
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+</div>
+<section class="s1">
+  <button id="copy" class="but">Copy</button>
+
+  <button id="color" class = "but">Background Color</button>
+
+  <button id="title" class = "but">Change Title</button>
+
+  <button id= "delete" class = "but">Delete</button>
+</section>
+
+
+<section class="s1" id="base">
+  <br>
+  <h1 class="h1" id="name">
+    Dog Facts
+  </h1>
+  <h2 class="h2">
+    Borzoi
+  </h2>
+
+  </br><img class="i1" src="https://imagesvc.meredithcorp.io/v3/mm/image?q=60&c=sc&poi=%5B700%2C759%5D&w=2000&h=1000&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F47%2F2021%2F08%2F05%2Fborzoi-is-big-tiktok-535986771-2000.jpg" width='300'/>
+
+  <p class="p1" id="desc">
+  This is a borzoi, they are a funny dog commonly found on the internet. They have been really popular recently. Here are some interesting "true" facts about them.
+  <ul class="l1">
+    <li>They have long noses</li>
+    <li>They look like horses</li>
+    <li>They can see the future</li>
+  </ul>
   </p>
 
-  <button type="button" @click="state.count++">count is: {{ state.count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+
+  <button class="but" id="details">
+    Details
+  </button>
+
+</section>
 </template>
 
 <script setup>
